ce10ae870068c45217c3807d210c0f00c8e22103
“dsoper2” <dsoper@cisco.com>
2024-04-30 10:45:11 -0500

    Add profile action so API calls deploy and activate

--- profile-examples/deploy-activate-server-profile.ps1
+++ profile-examples/deploy-activate-server-profile.ps1
@@ -4,8 +4,9 @@
 $Organization1 = Get-IntersightOrganizationOrganization -Name Demo-DevNet | Get-IntersightMoMoRef
 
 $getMo1 = Get-IntersightServerProfile -Name "SJC07-R14" -Organization $Organization1
-$ScheduledActions11 = Initialize-IntersightPolicyScheduledAction -Action "Deploy" -ObjectType "PolicyScheduledAction" -ClassId "PolicyScheduledAction"
-$ScheduledActions21 = Initialize-IntersightPolicyScheduledAction -Action "Activate" -ProceedOnReboot $true -ObjectType "PolicyScheduledAction" -ClassId "PolicyScheduledAction"
+$ScheduledActions11 = Initialize-IntersightPolicyScheduledAction -Action "Deploy"
+$ScheduledActions21 = Initialize-IntersightPolicyScheduledAction -Action "Activate" -ProceedOnReboot $true
 
-$mo1 = $getMo1 | Set-IntersightServerProfile -ScheduledActions @($ScheduledActions11,$ScheduledActions21)
+$mo1 = $getMo1 | Set-IntersightServerProfile -ScheduledActions @($ScheduledActions11,$ScheduledActions21) -Action "Deploy"
+
 $mo1
